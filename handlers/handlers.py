bb5bacdac6a76f449033a7ccd67bc9323423a9ce
Aleksandr Pakhomovskii <pakhomovskii@gmail.com>
2023-07-07 17:56:00 +0700

    some fixes

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -149,7 +149,7 @@ async def show_pop_up(update: Update, context: ContextTypes.DEFAULT_TYPE, text=N
 
             if reply_text:
                 text = "Новый заказ:\n\n" + reply_text + f"@{user_tg_name[0][0]}"
-                await context.bot.send_message(chat_id=557195190, text=text, )
+                await context.bot.send_message(chat_id=557195190, text=text)
                 await context.bot.answer_callback_query(callback_query_id=query.id,
                                                         text="""Ваша анкета была успешно оправлена и скоро с вами
                                                          свяжется наш специалист.\n\n
--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
+docker stop pro-novo-bot-container
+
+docker rm pro-novo-bot-container
+
 docker build -t pro-novo-bot .
 
 docker run -d --name pro-novo-bot-container pro-novo-bot
