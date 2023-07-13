bfa49c3570d2bb962811e7c9c9f44e5a085ab269
Aleksandr Pakhomovskii <pakhomovskii@gmail.com>
2023-07-13 10:50:31 +0700

    some fixes

--- script.sh
+++ script.sh
@@ -6,5 +6,4 @@ docker build -t pro-novo-bot .
 
 #docker run -d -it -p 5432:5432 -v pro-novo_volume:/data --name pro-novo-bot-container pro-novo-bot
 
-docker run -d -it -p 5432:5432 -v --mount type=bind,source=/var/lib/docker/volumes/pro-novo_volume/_data,target=/home/can,readonly alpine
-
+docker run -d -it -p 5432:5432 -v /var/lib/docker/volumes/pro-novo_volume/_data:/home/can,readonly alpine
