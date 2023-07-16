c18ad002906ec58e6e6350afafe17bea5d17a269
Aleksandr Pakhomovskii <pakhomovskii@gmail.com>
2023-07-16 11:11:49 +0700

    change db

--- script.sh
+++ script.sh
@@ -4,8 +4,9 @@ docker rm pro-novo-bot-container
 
 docker build -t pro-novo-bot .
 
-docker run -d  --name pro-novo-bot-container --add-host=database:209.38.224.54
+docker run -d  --name pro-novo-bot-container docker run --add-host=database:209.38.224.54 pro-novo-bot
 
+docker logs pro-novo-bot-container
 
 #docker run -d -it -p 5432:5432 -v /var/lib/docker/volumes/pro-novo_volume/_data/mydatabase.db:/home/can/mydatabase.db:ro alpine chown can:can /home/can/mydatabase.db
 #
