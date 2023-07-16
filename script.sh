478247525b89e418b65e615e661ed3f7d01b08c7
Aleksandr Pakhomovskii <pakhomovskii@gmail.com>
2023-07-16 11:23:05 +0700

    change db

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ docker rm pro-novo-bot-container
 
 docker build -t pro-novo-bot .
 
-docker run -d  --name pro-novo-bot-container docker run pro-novo-bot
+docker run -d  --name pro-novo-bot-container pro-novo-bot
 
 docker logs pro-novo-bot-container
 
